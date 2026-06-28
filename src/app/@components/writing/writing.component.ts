59725ab5041254a721910c8706241889a6ad37ed
Pei Lin <a0936856077@gmail.com>
2026-06-28 21:45:01 +0800

    about page fix the tsconfig

---
+++ src/app/@components/writing/writing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-writing',
+  imports: [],
+  templateUrl: './writing.component.html',
+  styleUrl: './writing.component.scss'
+})
+export class WritingComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './@components/homepage/homepage.component';
+import { AboutComponent } from './@components/about/about.component';
+import { WorksComponent } from './@components/works/works.component';
+import { WritingComponent } from './@components/writing/writing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'homepage',
+    pathMatch: 'full'
+  },
+  {
+    path: 'homepage',
+    component: HomepageComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'works',
+    component: WorksComponent
+  },
+  {
+    path: 'writing',
+    component: WritingComponent
+  }
+];
